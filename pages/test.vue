5bba358b3a27ffff60e0648576d7fa3d8c22161d
Benoit Martel <martel.b@gmail.com>
2021-09-08 17:15:06 -0400

    working animation

---
+++ pages/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    let arr = [1, 2, 3, 4, 5, 6];
+    console.log(this.shuffle(arr, 12));
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
